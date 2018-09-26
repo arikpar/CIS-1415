06906bdebbe35d4d704b7cc85f16e65d8c72dbfe
arikpar <43591314+arikpar@users.noreply.github.com>
2018-09-25 19:53:21 -0500

    Update 4.12.py

--- 4.12.py
+++ 4.12.py
@@ -14,10 +14,12 @@ print('Oil change -- $35\n'
       'Car wash -- $7\n'
       'Car wax -- $12')
 
-
+#gets input for selected services
 service1 = input('\nSelect first service:\n')
 service2 = input('Select second service:\n')
 
+#Outputs both services (if its on the menu) and their prices
+#if a '-' or nonexistent service is entered, will output 'No service'
 print("\nDavy's auto shop invoice")
 if service1 == 'Oil change' or service1 == 'Tire rotation' or service1 == 'Car wash' or service1 == 'Car wax':
     print('\nService 1: %s, $%d' % (service1, services[service1]))
@@ -29,5 +31,6 @@ if service2 == 'Oil change' or service2 == 'Tire rotation' or service2 == 'Car w
 else:
     print('Service 2: No service')
     
+#calculates and outputs total cost for services
 total = services[service1] + services[service2]
 print('\nTotal: $%d' % total)
