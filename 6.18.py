875dd1cc8dcbf0ebcd471c3d320290c74c9be735
arikpar <43591314+arikpar@users.noreply.github.com>
2018-10-11 12:33:32 -0500

    Create 6.18.py

---
+++ 6.18.py
@@ -0,0 +1,22 @@
+#Arik Parenteau
+#
+
+def get_num_of_characters(inputStr):
+    return len(inputStr)
+def output_without_whitespace(string):
+    new_string = ''
+    i = 0
+    while i < len(string):
+        if string[i] != ' ' and string[i] != '  ':
+            new_string = new_string + string[i]
+        i = i + 1
+    return new_string
+
+if __name__ == '__main__':
+    sentence = input('Enter a sentence or phrase:\n')
+    char_count = get_num_of_characters(sentence)
+    nowhitespace = output_without_whitespace(sentence)
+    
+    print('\nYou entered:', sentence)
+    print('\nNumber of characters:', char_count)
+    print('String with no whitespace:', nowhitespace)
