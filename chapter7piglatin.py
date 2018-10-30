bd2456f4824e56eaf21eb6fc2b7421222cb8cd69
gabesore <43591309+gabesore@users.noreply.github.com>
2018-10-30 14:52:48 -0500

    Create chapter7piglatin.py

---
+++ chapter7piglatin.py
@@ -0,0 +1,8 @@
+def main():
+    english = input('Enter your English here: ')
+    pig = []
+    for word in english.split():
+        pig.append(word[1:]+word[0]+'AY')
+    translated = 'Pig Latin: ' + ' '.join(pig)
+    print(translated.upper())
+main()
