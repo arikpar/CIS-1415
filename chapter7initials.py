031e5c0fd0c530ce6dee6679d271bec28d0ab90a
arikpar <43591314+arikpar@users.noreply.github.com>
2018-10-30 13:47:50 -0500

    Create chapter7initials.py

---
+++ chapter7initials.py
@@ -0,0 +1,8 @@
+fullname = input('Enter a full name\n')
+namelist = fullname.split()
+
+initial1 = namelist[0][0].upper()
+initial2 = namelist[1][0].upper()
+initial3 = namelist[2][0].upper()
+
+print('The initials are: %s. %s. %s' % (initial1,initial2,initial3))
