ac46b04c90b894a3f7d8225ec4521ede5621d32f
gabesore <43591309+gabesore@users.noreply.github.com>
2018-10-30 14:09:09 -0500

    Create chapter7vowels.py

---
+++ chapter7vowels.py
@@ -0,0 +1,30 @@
+def main():
+	mystr = input('Enter a string: ')
+	mystr.lower()
+	vowelSet = set(['a', 'e', 'i', 'o', 'u'])
+	runVowels(mystr, vowelSet)
+	runConsonants (mystr, vowelSet)
+	
+def runVowels(mystr, vowelSet):
+	index = 0
+	vowels = 0
+	
+	while index < len(mystr):
+		if mystr[index] in vowelSet:
+			vowels += 1
+			
+		index += 1
+	print ('This string consists of ', vowels, 'vowels.')
+		
+def runConsonants(mystr, vowelSet):
+	index = 0
+	consonants = 0
+	
+	while index < len(mystr):
+		if mystr[index] not in vowelSet:
+			consonants += 1
+			
+		index += 1
+	print ('This string consists of ', consonants, 'consonants.')
+	
+main()
