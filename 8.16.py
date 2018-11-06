3ab94ca2eea73f96f4829f4d5347ccc9a220be85
arikpar <43591314+arikpar@users.noreply.github.com>
2018-11-06 13:58:20 -0600

    Create 8.16.py

---
+++ 8.16.py
@@ -0,0 +1,36 @@
+#Arik Parenteau
+#
+weights = []
+
+def calculateAvg():
+    listSum = sum(weights)
+    return listSum / len(weights)
+
+def calculateKilo(pounds):
+    return pounds / 2.2
+
+def createList():
+    for x in range(1,5):
+        print('Enter weight %d:' % x)
+        weight = float(input())
+        weights.append(weight)
+    print('Weights:', weights)
+
+def printKilo():
+    index = int(input('\nEnter a list index (1 - 4):\n'))
+    print('Weight in pounds: %.2f' % weights[index - 1])
+    kilo = calculateKilo(weights[index - 1])
+    print('Weight in kilograms: %.2f' % kilo)
+    
+
+createList()
+average = calculateAvg()
+maxList = max(weights)
+print('\nAverage weight: %.2f' % average)
+print('Max weight: %.2f' % maxList)
+
+printKilo()
+
+print()
+weights.sort()
+print('Sorted list:', weights)
